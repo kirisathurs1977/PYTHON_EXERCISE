6255e03016c68062cf35cd16a46b621d9b94b9c8
kirisathurs1977 <sritharankirisathurs@gmail.com>
2024-06-12 11:36:59 +0530

    Find numbers which are divisible by 7 and multiple of 5 between a range

--- EX_1.py
+++ EX_1.py
@@ -1,1 +1,13 @@
 print("welcome python practice")
+# Create an empty list to store numbers that meet the given conditions
+nl = []
+
+# Iterate through numbers from 1500 to 2700 (inclusive)
+for x in range(1500, 2701):
+    # Check if the number is divisible by 7 and 5 without any remainder
+    if (x % 7 == 0) and (x % 5 == 0):
+        # If the conditions are met, convert the number to a string and append it to the list
+        nl.append(str(x))
+
+# Join the numbers in the list with a comma and print the result
+print(','.join(nl))
